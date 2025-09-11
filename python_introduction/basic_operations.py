fa7b9f2ad307905ddf9500936cdc12f363d5947b
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-09-11 23:08:38 +0100

    changed sum to addition

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,11 +3,11 @@ number1 = 10
 number2 = 5
 
 # Perform operations
-sum = number1 + number2
+addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
 
 # Print results in required format
-print(f"sum of {number1} and {number2} is {sum}")
+print(f"addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
 print(f"Multiplication of {number1} and {number2} is {multiplication}")
