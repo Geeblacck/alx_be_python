3d218c8176d4403ce65062bcbf18d1f57c73ad94
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-09-11 23:40:52 +0100

    I added Correct variable assignments for principal, rate, and time

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-principal = float (1000) #(representing $1000)
-rate = float (0.05) #(representing 5% annual interest rate)
-time = float (3) #(representing 3 years)
+principal = 1000 #(representing $1000)
+rate = 0.05 #(representing 5% annual interest rate)
+time = 3 #(representing 3 years)
 #Calculate the simple interest using the formula provided and store the result in a variable named interest.
 interest = (principal * rate * time)
 print (f"The simple interest is: {interest}")
