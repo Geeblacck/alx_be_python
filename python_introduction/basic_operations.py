801f31e71dac7bd167ca2dc56038f1c39ce2bac2
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-09-11 22:58:04 +0100

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Assign values to variables
+number1 = 10
+number2 = 5
+
+# Perform operations
+sum = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print results in required format
+print(f"sum of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
