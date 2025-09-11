188d489c2fe2d99966d04545ffdd59d2f3357f87
Oluwagbogo Toluwase Oladimeji <oladimejioluwagbogo@gmail.com>
2025-09-11 23:51:14 +0100

    Initial commit

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,5 +1,5 @@
 length = 10
 width = 5
 #Calculate the area of the rectangle
-Area = length * width
-print (f"The area of the rectangle is: {Area}")
+area = length * width
+print (f"The area of the rectangle is: {area}")
